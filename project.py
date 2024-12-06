bd7e9463ab3fd073a2b7fde4f4e71cd3ba89fd6a
Sticarto <123905512+sticarto@users.noreply.github.com>
2024-12-06 15:11:04 -0800

    Change data source and create datasets

--- project.py
+++ project.py
@@ -5,7 +5,34 @@ import kagglehub
 
 # Downloads images directly from Kaggle
 # Download latest version
-path = kagglehub.dataset_download("alessiocorrado99/animals10")
+path = kagglehub.dataset_download("andrewmvd/medical-mnist")
 
 print("Path to dataset files:", path)
+
+
+# Create dataset
+BATCHSIZE = 64
+IMG_HEIGHT = 64
+IMG_WIDTH = 64
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    path,
+    validation_split = 0.2,
+    subset = "training",
+    seed = 123,
+    image_size = (IMG_HEIGHT, IMG_WIDTH),
+    batch_size = BATCHSIZE
+)
+
+validation_ds = tf.keras.utils.image_dataset_from_directory(
+    path,
+    validation_split = 0.2,
+    subset = "validation",
+    seed = 123,
+    image_size = (IMG_HEIGHT, IMG_WIDTH),
+    batch_size = BATCHSIZE
+)
+
+# CNN Model
+model = keras.Sequential()
 
