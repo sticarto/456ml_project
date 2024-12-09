8c3b972e94525ab5a1ccd9ebe3969c6302814efb
Sticarto <123905512+sticarto@users.noreply.github.com>
2024-12-09 14:58:26 -0800

    Build the basic skeleton code for CNN

--- project.py
+++ project.py
@@ -15,7 +15,7 @@ BATCHSIZE = 64
 IMG_HEIGHT = 64
 IMG_WIDTH = 64
 
-train_ds = tf.keras.utils.image_dataset_from_directory(
+train_ds = keras.utils.image_dataset_from_directory(
     path,
     validation_split = 0.2,
     subset = "training",
@@ -24,7 +24,7 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
     batch_size = BATCHSIZE
 )
 
-validation_ds = tf.keras.utils.image_dataset_from_directory(
+validation_ds = keras.utils.image_dataset_from_directory(
     path,
     validation_split = 0.2,
     subset = "validation",
@@ -33,6 +33,34 @@ validation_ds = tf.keras.utils.image_dataset_from_directory(
     batch_size = BATCHSIZE
 )
 
-# CNN Model
-model = keras.Sequential()
+class_names = train_ds.class_names
 
+# FIXME: Somehow, I need the datasets to be in a numpy array unless there is another way.
+for image_batch, labels_batch in train_ds:
+    print(image_batch.shape)
+    print(labels_batch.shape)
+    break
+
+
+
+# CNN Model
+# model = keras.models.Sequential()
+# model.add(keras.layers.Conv2D())
+
+# model.add(keras.layers.Flatten())
+
+# model.add(keras.layers.Dense())
+# model.add(keras.layers.Dense(units = 6, activation="softmax"))
+
+
+# model.compile(
+#     optimizer = "adam",
+#     loss="sparse_categorical_crossentropy",
+#     metrics=["accuracy"]
+# )
+
+# history = model.fit()
+
+# score = model.evaluate()
+
+# MAKE PREDICTIONS
